05593ff5186fc27c8318cd854deaae64e6e5f761
MAGIJIG <magijigu@gmail.com>
2019-06-13 22:23:48 +0900

    Update 20190612132031_create_products.rb

--- db/migrate/20190612132031_create_products.rb
+++ db/migrate/20190612132031_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
       t.string :name, null: false, :limit=> 40,index: true 
-      t.integer :price, null: false, :limit => 9999999
+      t.string :price, null: false, :limit => 9999999
       t.string :size, null: false 
       t.string :description, null: false, :limit=> 1000 
       t.integer :favorite, null: false 
