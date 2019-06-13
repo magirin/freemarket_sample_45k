e8516aff871f857165b6c574a29bf1dd0bf28b53
MAGIJIG <magijigu@gmail.com>
2019-06-13 22:20:29 +0900

    add space into terms
    null:false=>null: false

--- db/migrate/20190612131736_create_reputations.rb
+++ db/migrate/20190612131736_create_reputations.rb
@@ -1,8 +1,8 @@
 class CreateReputations < ActiveRecord::Migration[5.0]
   def change
     create_table :reputations do |t|
-      t.references :user, null:false
-      t.integer :status, null:false
+      t.references :user, null: false
+      t.integer :status, null: false
       t.timestamps
     end
   end
--- db/migrate/20190613000945_create_sold_products.rb
+++ db/migrate/20190613000945_create_sold_products.rb
@@ -1,8 +1,8 @@
 class CreateSoldProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :sold_products do |t|
-      t.references :product, null:false
-      t.references :user, null:false
+      t.references :product, null: false
+      t.references :user, null: false
       t.timestamps
     end
   end
