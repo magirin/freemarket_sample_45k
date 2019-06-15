00f073e2fdddb17a9c5b922e2a7a8d9eb90cfcc5
MAGIJIG <magijigu@gmail.com>
2019-06-15 19:26:30 +0900

    make redirecct to sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,3 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#new'
 end
-
-def index
-end
-        
-def new
-end
-
-def create
-end
-
-def edit
-end
-
-def show
-end
-        
-def update
-end
-
-def destroy
-end
